2ca6c065f2b9b3231404837a4ec101dff84bcfa4
Erik Haliewicz <ehal256@gmail.com>
2022-05-16 23:12:26 -0700

    fix untagged/none logic bug

--- todo/views.py
+++ todo/views.py
@@ -192,7 +192,7 @@ def get_tag_info(todo_logs):
     for log in todo_logs:
         time_lookup[log.tag] += log.duration
     for (tag,cnt) in tag_counts.items():
-        if tag == "":
+        if tag == "" or tag is None:
             yield ("untagged",cnt,get_hr_min(time_lookup[tag]))
         else:
             yield (tag,cnt,get_hr_min(time_lookup[tag]))
