3edc2ef0999cf38abceaeadd9b9df12eff96c1a5
Erik Haliewicz <erik@gomerits.com>
2022-05-18 10:38:30 -0700

    add logging

--- app/settings.py
+++ app/settings.py
@@ -127,21 +127,6 @@ LOGGING = {
     },
 }
 
-LOGGING = {
-    'version': 1,
-    'disable_existing_loggers': False,
-    'handlers': {
-        'console': {
-            'class': 'logging.StreamHandler',
-        },
-    },
-    'root': {
-        'handlers': ['console'],
-        'level': 'WARNING',
-    },
-}
-
-
 # Internationalization
 # https://docs.djangoproject.com/en/2.0/topics/i18n/
 
