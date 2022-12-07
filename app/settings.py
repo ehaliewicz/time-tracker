6e4d05fc9834a22059c493115b3d7a1dc8811ad7
Erik Haliewicz <erik@gomerits.com>
2022-12-07 10:22:59 -0800

    fix syntax error

--- app/settings.py
+++ app/settings.py
@@ -65,7 +65,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
-    "django.middleware.gzip.GZipMiddleware"
+    "django.middleware.gzip.GZipMiddleware",
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
