52136e9bf49928ce6b94ee69e07f8e76a54ec91c
Erik Haliewicz <ehal256@gmail.com>
2023-05-12 00:53:59 -0700

    undo silk and debugging

--- app/urls.py
+++ app/urls.py
@@ -70,7 +70,5 @@ urlpatterns = [
     path("api/pause_timer/<int:log_id>", todo.api.pause_timer, name="api pause timer for log"),
     path("api/resume_timer/<int:log_id>", todo.api.resume_timer, name="resume timer for log"),
     path("api/stop_timer/<int:log_id>", todo.api.stop_timer, name="api stop timer for log"),
-    path("api/delete_timer/<int:log_id>", todo.api.delete_timer, name="api elete timer for log"),
-    path('silk/', include('silk.urls', namespace='silk')),
-    
+    path("api/delete_timer/<int:log_id>", todo.api.delete_timer, name="api elete timer for log"),    
 ]
